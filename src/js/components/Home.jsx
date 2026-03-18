9fac4782f929d1bf12a8465a79d3bba8cf6acff8
erikaportes <152933693+erikaportes@users.noreply.github.com>
2026-03-18 13:53:29 +0000

    todo list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,58 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const ToDoList = () => {
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [inputList, setInputList] = useState("")
+	const [listaTareas, setListaTareas] = useState([])
+
+	function crearTarea(Enter) {
+		if (Enter === "Enter" && inputList.trim() !== "") {
+			setListaTareas([...listaTareas, inputList])
+			setInputList("")
+		}
+	}
+
+	function trash(indice) {
+		setListaTareas(listaTareas.filter((item, i) => indice != i))
+	}
+
+	return (
+
+		<>
+			<h1>My Todos</h1>
+			<div className="container">
+
+				<input
+					type="text"
+					placeholder='What do you need to do?'
+					value={inputList}
+					onChange={(e) => setInputList(e.target.value)}
+					onKeyUp={evento => crearTarea(evento.key)}
+				/>
+				<ul className="list-group">
+					{listaTareas.map((tarea, indice) => {
+						return (
+							<li
+								className="list-group-item d-flex justify-content-between align-items-center tarea-item"
+								key={tarea + indice} >
+								{tarea}
+								<i
+									onClick={() => trash(indice)}
+									className="fa-solid fa-trash-can trash-icon ms-auto">
+								</i>
+							</li>
+						)
+					})
+					}
+				</ul>
+				<div>{listaTareas.length} tasks</div>
+
+			</div>
+
+		</>
+
+	)
 };
 
-export default Home;
+export default ToDoList;
